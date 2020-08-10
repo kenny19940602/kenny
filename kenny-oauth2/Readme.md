b80574be20c69873105a8c892cd8fd8767421189
金龙-汇码科技 <kenny19940602@163.com>
2020-08-10 20:54:31 +0800

    first commit

---
+++ kenny-oauth2/Readme.md
@@ -0,0 +1,1 @@
+# 微服务权限终极解决方案，Spring Cloud Gateway + Oauth2 实现统一认证和鉴权！
---
+++ kenny-oauth2/kenny-oauth2-gateway/src/main/java/com/kenny/cloud/api/IErrorCode.java
@@ -0,0 +1,10 @@
+package com.kenny.cloud.api;
+
+/**
+ * 封装API的错误码
+ */
+public interface IErrorCode {
+    long getCode();
+
+    String getMessage();
+}
